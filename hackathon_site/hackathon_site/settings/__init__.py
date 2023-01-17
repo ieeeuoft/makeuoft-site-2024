a27b88e10f3242ee9d10e01f9609a57d443df885
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2023-01-17 12:56:23 -0500

    fix: add the email configuration (#13)

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -67,7 +67,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT", None)
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", None)
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", None)
 EMAIL_USE_SSL = True
-DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_FROM_ADDRESS", "webmaster@localhost")
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_FROM_ADDRESS", "hello@makeuoft.ca")
 CORS_ALLOW_CREDENTIALS = True
 
 RECAPTCHA_DOMAIN = "www.recaptcha.net"
@@ -308,7 +308,7 @@ LOGGING = {
 
 # Event specific settings
 HACKATHON_NAME = "MakeUofT"
-DEFAULT_FROM_EMAIL = "webmaster@ieee.utoronto.ca"
+DEFAULT_FROM_EMAIL = "hello@makeuoft.ca"
 CONTACT_EMAIL = "hello@makeuoft.ca"
 HSS_ADMIN_EMAIL = "hardware@makeuoft.ca"
 
