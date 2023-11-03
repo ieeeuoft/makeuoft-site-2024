786277352d0e57cc9fc0d9a67801599ffdc3a072
Luke Cheseldine <cheeseldine@gmail.com>
2023-11-03 14:50:17 -0400

    url

--- hackathon_site/dashboard/frontend/src/constants.js
+++ hackathon_site/dashboard/frontend/src/constants.js
@@ -5,3 +5,4 @@ export const maxTeamSize = 4;
 export const hardwareSignOutStartDate = new Date(2024, 2, 1, 0, 0);
 export const hardwareSignOutEndDate = new Date(2024, 2, 18, 0, 0);
 export const hssTestUserGroup = "HSS Test Users";
+export const serverUrl = "https://makeuoft.ca";
