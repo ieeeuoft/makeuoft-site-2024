55bf8fe60a1d8cb0d19193e4ba0ec8f6abe6cc16
Samuel Liu <liusamuelt@gmail.com>
2024-01-05 17:53:27 -0500

    prettier

--- hackathon_site/event/static/event/js/application.js
+++ hackathon_site/event/static/event/js/application.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
-    $("#what_hackathon_experience, #id_why_participate, #id_what_technical_experience").characterCounter();
+    $(
+        "#what_hackathon_experience, #id_why_participate, #id_what_technical_experience",
+    ).characterCounter();
 });
